f4e76de43b556e81827c4531db4dc1acfac3fbdb
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-09-13 23:22:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print with New Line/print-with-new-line.java
@@ -0,0 +1,9 @@
+class GFG {
+    public static void main(String[] args) {
+
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+
+    }
+}
