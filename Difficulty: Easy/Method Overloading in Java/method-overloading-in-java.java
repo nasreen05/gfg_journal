895d3a97afb5f85718db0a2277bbeadc572ab8ee
Reshma Nasreen <reshmanasreen30@gmail.com>
2026-09-16 23:38:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Method Overloading in Java/method-overloading-in-java.java
@@ -0,0 +1,17 @@
+class Calculator {
+
+    // Sum of two integers
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    // Sum of three integers
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Sum of two double values
+    double sum(double x, double y) {
+        return x + y;
+    }
+}
